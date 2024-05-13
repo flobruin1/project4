54779a2f3e1703961cc28e2f03cc743032fd1119
flobruin1 <tranlevi01@gmail.com>
2024-05-13 16:52:59 +0700

    hello

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = '4knruxwlp7';
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-2.amazonaws.com/dev`
+
+export const authConfig = {
+  domain: 'dev-6av2eol55qqdqh3k.us.auth0.com',
+  clientId: 'UhathMYOcwWcPcYlL3YmMnMaTGEVtSHG',
+  callbackUrl: 'http://localhost:3000/callback'
+};
